0bfaac82cce02ffdc21e6652f140aa866ff8f03d
Erick Reyes <reyespoloerick1@gmail.com>
2024-06-13 15:59:49 -0500

    Agregue Cambio de Tema

--- Registro/sesion.js
+++ Registro/sesion.js
@@ -1,10 +1,10 @@
-document.getElementById("CambioTema").addEventListener("click", function() {
+document.getElementById("CambioTema").querySelector('.input').addEventListener("change", function() {
     document.body.classList.toggle("tema-oscuro");
     document.body.classList.toggle("tema-claro");
-});
-window.onload = function(){
-    if(!document.body.classList.contains("tema-claro") && 
-        !document.body.classList.contains("tema-oscuro")){
-        document.body.classList.add("tema-claro");
+  });
+  
+  window.onload = function() {
+    if (!document.body.classList.contains("tema-claro") && !document.body.classList.contains("tema-oscuro")) {
+      document.body.classList.add("tema-claro");
     }
-};
+  };
