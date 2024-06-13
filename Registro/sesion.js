6498cf7c425663f5cdacb05d9bd9e212a1e5c981
Erick Reyes <reyespoloerick1@gmail.com>
2024-06-13 14:56:38 -0500

    Cambio de diseño Registro Clientes

--- Registro/sesion.js
+++ Registro/sesion.js
@@ -8,18 +8,3 @@ window.onload = function(){
         document.body.classList.add("tema-claro");
     }
 };
-
-/*
-document.getElementById('theme-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme');
-    document.body.classList.toggle('light-theme');
-});
-
-// Configuración inicial del tema
-window.onload = function() {
-    if (!document.body.classList.contains('light-theme') && 
-        !document.body.classList.contains('dark-theme')) {
-        document.body.classList.add('light-theme');
-    }
-};
-*/
