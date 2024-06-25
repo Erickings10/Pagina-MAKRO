12b667a1c55e27c610fef88fc7376f33e17ffd75
hol10022002@gmail.com <hol10022002@gmail.com>
2024-06-25 01:16:46 -0500

    solo falta agregar mas img

--- idk.js
+++ idk.js
@@ -1,29 +1,45 @@
 document.addEventListener('DOMContentLoaded', function () {
     const products = {
         abarrotes: [
-            { name: 'Abarrote 1', image: '' },
-            { name: 'Abarrote 2', image: '' },
-            { name: 'Abarrote 3', image: 'img/alimentos/queso.png' },
-            { name: 'Abarrote 4', image: 'img/alimentos/yogurt.png' },
-            { name: 'Abarrote 5', image: '' }
+            { name: 'Abarrote 1', image: 'img/alimentos/atun.png' },
+            { name: 'Abarrote 2', image: 'img/alimentos/mayonesa.png' },
+            { name: 'Abarrote 3', image: 'img/alimentos/fideos.png' },
+            { name: 'Abarrote 4', image: 'img/alimentos/mermelada.png' }
         ],
         bebidas: [
-            { name: 'Bebida 1', image: 'img/products/bebida1.png' },
-            { name: 'Bebida 2', image: 'img/products/bebida2.png' }
+            { name: 'Bebida 1', image: 'img/alimentos/gaseosa.png' },
+            { name: 'Bebida 2', image: 'img/alimentos/agua.png' },
+            { name: 'Bebida 3', image: 'img/alimentos/johnnie.png' },
+            { name: 'Bebida 4', image: 'img/alimentos/gatorade3.png' },
         ],
         lacteos: [
             { name: 'Lacteo 1', image: 'img/alimentos/Gloria.png' },
             { name: 'Lacteo 2', image: 'img/alimentos/condensada.png' },
             { name: 'Lacteo 3', image: 'img/alimentos/yogurt.png' },
-            { name: 'Lacteo 4', image: 'img/alimentos/helado2.png' }
+            { name: 'Lacteo 4', image: 'img/alimentos/helado2.png' },
+            { name: 'Abarrote 3', image: 'img/alimentos/fideos.png' },
+            { name: 'Abarrote 4', image: 'img/alimentos/yogurt.png' },
+            { name: 'Abarrote 3', image: 'img/alimentos/fideos.png' },
+            { name: 'Abarrote 4', image: 'img/alimentos/yogurt.png' },
+            { name: 'Abarrote 3', image: 'img/alimentos/fideos.png' },
+            { name: 'Abarrote 4', image: 'img/alimentos/yogurt.png' },
+            { name: 'Abarrote 3', image: 'img/alimentos/fideos.png' },
+            { name: 'Abarrote 4', image: 'img/alimentos/yogurt.png' },
+            { name: 'Abarrote 3', image: 'img/alimentos/fideos.png' },
+            { name: 'Abarrote 4', image: 'img/alimentos/yogurt.png' }
         ],
         confiteria: [
-            { name: 'Confiteria 1', image: 'img/products/confiteria1.png' },
-            { name: 'Confiteria 2', image: 'img/products/confiteria2.png' }
+            { name: 'Confiteria 1', image: 'img/alimentos/picaras.png' },
+            { name: 'Confiteria 2', image: 'img/alimentos/casino.png' },
+            { name: 'Confiteria 3', image: 'img/alimentos/nik.png' },
+            { name: 'Confiteria 4', image: 'img/alimentos/donapepa.png' },
         ],
         limpieza: [
-            { name: 'Limpieza 1', image: 'img/products/limpieza1.png' },
-            { name: 'Limpieza 2', image: 'img/products/limpieza2.png' }
+            { name: 'Limpieza 1', image: 'img/alimentos/mrmusculo.png' },
+            { name: 'Limpieza 2', image: 'img/alimentos/blanc.png' },
+            { name: 'Limpieza 3', image: 'img/alimentos/pato.png' },
+            { name: 'Limpieza 4', image: 'img/alimentos/clorox.png' },
+
         ]
         };
     
